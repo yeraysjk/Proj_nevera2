793d819add650ed78deedff10a2e4e47861b386f
marc_14 <retilla_20@hotmail.com>
2023-05-26 15:35:56 +0200

    classe fitxer,producte(crear mostrar etc)

--- src/main/java/com/example/proj_nevera/Fitxers.java
+++ src/main/java/com/example/proj_nevera/Fitxers.java
@@ -9,11 +9,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * Una classe fitxers per crear, modificar i eliminar
+ */
 public class Fitxers {
 
     Fitxers() {
     }
 
+    /**
+     * Escriu fitxer en un text.
+     *
+     * @param arxiu  l'arxiu on es guarda el text
+     * @param text   el text que s'afegirà
+     * @param afegir true per afegir text o false per sobreescriure
+     * @throws IOException the io exception
+     */
     public void escriuFitxerText(String arxiu, String text, Boolean afegir) throws IOException {
         FileWriter out = new FileWriter(arxiu, afegir);
         Throwable var5 = null;
@@ -40,6 +51,13 @@ public class Fitxers {
 
     }
 
+    /**
+     * Retorna un fitxer de text a un string.
+     *
+     * @param fitxer el nom del fitxer o el seu camí
+     * @return un string d'una línia
+     * @throws IOException the io exception
+     */
     public String retornaFitxerText(String fitxer) throws IOException {
         String linia = "";
         FileReader in = new FileReader(fitxer);
@@ -69,9 +87,25 @@ public class Fitxers {
         return linia;
     }
 
+    /**
+     * Mostra fitxer text per consola.
+     *
+     * @param fitxer el nom del fitxer o el seu camí
+     * @throws IOException the io exception
+     */
+    //@Override
     public void mostraFitxerText(String fitxer) throws IOException {
         System.out.println(this.retornaFitxerText(fitxer));
     }
+
+    /**
+     * Escriu un objecte en un fitxer.
+     *
+     * @param obj    l'obj que volem pasar les dades a un fitxer
+     * @param arxiu  l'arxiu on el volem guardar
+     * @param afegir true per afegir false per sobreescriure
+     * @throws IOException the io exception
+     */
     public void escriuObjecteFitxer(Object obj, String arxiu,boolean afegir)throws IOException{
 
         if(!afegir || !existeix(arxiu)){
@@ -83,6 +117,13 @@ public class Fitxers {
         }
     }
 
+    /**
+     * Retorna fitxer objecte en una llista list.
+     *
+     * @param fitxer la ruta del fitxer
+     * @return the list retorna en una llista l'objecte
+     * @throws ClassNotFoundException the class not found exception
+     */
     public List<Object> retornaFitxerObjecteEnLlista(String fitxer)throws  ClassNotFoundException{
         List<Object> LObjs =new ArrayList<>();
         try{
@@ -100,6 +141,12 @@ public class Fitxers {
         return  LObjs;
     }
 
+    /**
+     * Existeix boolean.
+     *
+     * @param ruta the ruta
+     * @return the boolean
+     */
     public boolean existeix(String ruta) {
         // Comprova la existència d'un directori o un fitxer
         Path fitxer = Paths.get(ruta);         // Path --> import java.nio.file.Path;
--- src/main/java/com/example/proj_nevera/HelloController.java
+++ src/main/java/com/example/proj_nevera/HelloController.java
@@ -41,24 +41,45 @@ public class HelloController {
      */
     public TextField nom_entrada;
 
+    /**
+     * The Mostrar producte.
+     */
     public Label mostrar_producte;
 
+    /**
+     * The Nom buscar.
+     */
     public TextField nom_buscar;
+    /**
+     * The Producte trobat.
+     */
     public Label producte_trobat;
 
 
+    /**
+     * Guardar producte en un fitxer amb el botó guardar i el mostra per la finestra
+     *
+     * @throws IOException the io exception
+     */
     public void BTguardar() throws IOException {
         String nom = nom_entrada.getText();
 
         double preu = Double.parseDouble(preu_entrada.getText());
         String descripcio = descripcio_entrada.getText();
         String data = String.valueOf(data_entrada.getValue());
-        mostrar_producte.setText("PRODUCTE AFEGIT \n"+nom +"\n"+ preu +"\n"+descripcio+"\n" + data);
+        mostrar_producte.setText("PRODUCTE AFEGIT \n" + nom + "\n" + preu + "\n" + descripcio + "\n" + data);
         Producte prod = new Producte(nom, preu, descripcio, data);
 
         prod.guardarProducteFitxer();
     }
 
+    /**
+     * Cercar producte per nom i el mostra per la finestra
+     *
+     * @throws ClassNotFoundException the class not found exception
+     * @throws FileNotFoundException  the file not found exception
+     * @throws InterruptedException   the interrupted exception
+     */
     public void cercarProducte() throws ClassNotFoundException, FileNotFoundException, InterruptedException {
         String nom = nom_buscar.getText();
         Producte producto = new Producte();
@@ -69,10 +90,10 @@ public class HelloController {
         for (Producte pr : lProductes) {
             // Compara el nombre del producto con el nombre buscado
             if (pr.getNom().equals(nom)) {
-                producte_trobat.setText("PRODUCTE TROBAT \n"+
-                        pr.getNom()+"\n"+
-                        pr.getPreu()+"\n"+
-                        pr.getDescripcio()+"\n"+
+                producte_trobat.setText("PRODUCTE TROBAT \n" +
+                        pr.getNom() + "\n" +
+                        pr.getPreu() + "\n" +
+                        pr.getDescripcio() + "\n" +
                         pr.getData());
             }
 
